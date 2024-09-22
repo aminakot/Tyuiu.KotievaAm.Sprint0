5a5db1c53e9cefc7e9da57622b47a97d0890bbc7
Tyuiu.KotievaAm@outlook.com <Tyuiu.KotievaAm@outlook.com>
2024-09-22 18:18:40 +0500

    Task1

---
+++ Tyuiu.KotievaAm.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.KotievaAm.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello,World");
+            string name = "Амина";
+            int age = 17;
+            Console.WriteLine("Здравстыуйте меня зовут" + name + "мне" + age + "лет!");
+            Console.ReadKey();
+        }
+    }
+}
